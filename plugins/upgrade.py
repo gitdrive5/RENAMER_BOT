f2c5fac23e19be60f52bbfdf51f11aa9a0f0cfaa
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-02-13 19:56:21 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -5,7 +5,7 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 4GB
+	Daily  Upload limit 2GB
 	Price 0
 	
 	**VIP 1 ** 
@@ -32,7 +32,7 @@ async def upgrade(bot,update):
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
 	text = """**Free User Plan**
-	Daily 🔺 Upload limit 4GB
+	Daily 🔺 Upload limit 2GB
 	Price 0
 	
 	**VIP 1 ** 
