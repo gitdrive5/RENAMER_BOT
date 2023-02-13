515b339a3d79a394a512bc235c6ae4a599c5d0f1
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-02-13 15:27:04 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit , usertype,addpre
-ADMIN = int(os.environ.get("ADMIN", 923943045))
+ADMIN = int(os.environ.get("ADMIN", ""))
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
 
@@ -29,20 +29,20 @@ async def buypremium(bot, message):
 async def vip1(bot,update):
 	id = update.message.reply_to_message.text.split("/addpremium")
 	user_id = id[1].replace(" ", "")
-	inlimit  = 10737418240
-	uploadlimit(int(user_id),10737418240)
+	inlimit  = 53687091200
+	uploadlimit(int(user_id),53687091200)
 	usertype(int(user_id),"VIP1")
 	addpre(int(user_id))
-	await update.message.edit("Added successfully To Premium Upload limit 10 GB")
+	await update.message.edit("Added successfully To Premium Upload limit 50 GB")
 	await bot.send_message(user_id,"Hey Ur Upgraded To VIP 1 check your plan here /myplan")
 
 @Client.on_callback_query(filters.regex('vip2'))
 async def vip2(bot,update):
 	id = update.message.reply_to_message.text.split("/addpremium")
 	user_id = id[1].replace(" ", "")
-	inlimit  = 53687091200
-	uploadlimit(int(user_id),53687091200)
+	inlimit  = 214748364800
+	uploadlimit(int(user_id),214748364800)
 	usertype(int(user_id),"VIP2")
 	addpre(int(user_id))
-	await update.message.edit("Added successfully To Premium Upload limit 50 GB")
+	await update.message.edit("Added successfully To Premium Upload limit 200 GB")
 	await bot.send_message(user_id,"Hey Ur Upgraded To VIP 2 check your plan here /myplan")
